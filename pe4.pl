af4511ff98b23cb02af4846776efd2d77d9d2625
Herman Westerman <58404234+namwe@users.noreply.github.com>
2020-12-09 20:23:03 +0100

    Remove junk code

--- pe4.pl
+++ pe4.pl
@@ -1,7 +1,5 @@
-onTheGuestlist(name, [name|restOfTheList]).
-onTheGuestList(name, [firstPerson|rest]) :- onTheGuestList(name, rest).
 
-
+%union where the elements are expected to be unique
 my_union([],x,y).
 my_union([X|A],C,[X|B]):- append(A,C,B).
   
